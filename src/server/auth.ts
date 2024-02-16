87842a0045d2b778a1732e6946b2d358611fbaa0
SwiichyCode <adlpromail@gmail.com>
2024-02-16 15:53:15 +0100

    ci: setup ci

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,7 +4,6 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import GithubProvider from "next-auth/providers/github";
 
 import { env } from "@/env";
