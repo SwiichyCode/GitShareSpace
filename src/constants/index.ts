578081a49ad7b2186320e78717c260f3a6c759bd
SwiichyCode <adlpromail@gmail.com>
2024-02-17 20:25:11 +0100

    feat: add error constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,11 @@
-export const URL_CONSTANTS = {
+export const URL = {
   HOME: "/",
   PROFILE: "/profile",
   SHARE: "/share",
   STARS: "/stars",
+};
+
+export const ERROR_MESSAGE = {
+  REPOSITORY_NOT_EXIST: "Repository does not exist",
+  REPOSITORY_ALREADY_EXIST: "Repository already exists",
 };
