2208c56b0e5947e588d8e1d216dcdfed1ee2628d
SwiichyCode <adlpromail@gmail.com>
2024-02-19 19:34:19 +0100

    fix: eslint error

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,14 +3,11 @@ import { AddRepositoryForm } from "@/components/organisms/_forms/addrepository.f
 import { DataSharingAgreementForm } from "@/components/organisms/_forms/dataSharingAgreement.form";
 import { RepositoryGrid } from "../organisms/RepositoryGrid";
 import userService from "@/services/user.service";
-import octokitService from "@/services/octokit.service";
 
 export const Home = async () => {
   const session = await getServerAuthSession();
   const user = session && (await userService.getUser(session.user.id));
 
-  const a = await octokitService.getStaredRepositoriesByUser("CorentinGS");
-  console.log(a!.data.length);
   return (
     <div className="p-8">
       <RepositoryGrid user={user} />
