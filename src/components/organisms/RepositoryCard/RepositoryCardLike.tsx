837b2bc5bb2bd59a7f8f3ce0650d01bff0971cd4
SwiichyCode <adlpromail@gmail.com>
2024-03-01 05:04:21 +0100

    refactor: extract optimisticLike and likeConfetti logic in custom hook

--- src/components/organisms/RepositoryCard/RepositoryCardLike.tsx
+++ src/components/organisms/RepositoryCard/RepositoryCardLike.tsx
@@ -1,15 +1,12 @@
-"use client";
-import { useState, useCallback, useOptimistic, useRef } from "react";
-import { HeartIcon, HeartFillIcon } from "@primer/octicons-react";
-import { likeOrUnlikeRepository } from "@/actions/likerepository.action";
-import { cn } from "@/lib/utils";
-import { handleLikeCount } from "@/lib/utils";
-import { formatNumber, hasLikedRepository } from "@/lib/utils";
-import type { Repository } from "@/types/prisma.type";
-import type { User } from "@/types/prisma.type";
-import type { Like } from "@prisma/client";
-
 import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+import { useOptimisticLike } from "@/hooks/useOptimisticLike";
+import { useLikeConfetti } from "@/hooks/useConfettiLike";
+import { HeartIcon, HeartFillIcon } from "@primer/octicons-react";
+import { handleLikeCount } from "@/lib/utils";
+import { formatNumber } from "@/lib/utils";
+import { cn } from "@/lib/utils";
+import type { Repository, User } from "@/types/prisma.type";
+import type { Like } from "@prisma/client";
 
 type Props = {
   user: User | null;
@@ -17,80 +14,14 @@ type Props = {
   repository: Repository;
 };
 
-type TRunAnimationParams = {
-  speed: number;
-  duration?: number;
-  delay?: number;
-};
-
-type TConductorInstance = {
-  run: (params: TRunAnimationParams) => void;
-  shoot: () => void;
-  pause: () => void;
-  stop: () => void;
-};
-
 export const RepositoryCardLike = ({ user, repository, likes }: Props) => {
-  const [isUpdating, setIsUpdating] = useState(false);
-  const [optimisticLikes, setOptimisticLikes] = useOptimistic(
-    likes,
-    (state, newLike: Like) => {
-      if (
-        state.some((like) => hasLikedRepository(like, user?.id, repository.id))
-      ) {
-        return state.filter(
-          (like) => !hasLikedRepository(like, user?.id, repository.id),
-        );
-      }
+  const { optimisticLikes, handleLikeRepository, isLiked, isUpdating } =
+    useOptimisticLike({ user, repository, likes });
 
-      return [...state, newLike];
-    },
-  );
-
-  const isLiked = optimisticLikes.some(
-    (like) => like.userId === user?.id && like.repositoryId === repository.id,
-  );
-
-  const handleLikeRepository = useCallback(
-    async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-
-      if (!user || isUpdating) return;
-
-      setIsUpdating(true);
-
-      try {
-        setOptimisticLikes({
-          userId: user.id,
-          repositoryId: repository.id,
-          id: Math.random(),
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        });
-
-        await likeOrUnlikeRepository({
-          repositoryId: repository.id,
-        });
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsUpdating(false);
-      }
-    },
-    [user, isUpdating, repository.id, setOptimisticLikes],
-  );
-
-  const controller = useRef<TConductorInstance | null>(null);
-
-  const onInitHandler = ({ conductor }: { conductor: TConductorInstance }) => {
-    controller.current = conductor;
-  };
-
-  const onShoot = () => {
-    controller.current?.shoot();
-  };
-
-  const handleShoot = isLiked && !isUpdating ? onShoot : undefined;
+  const { onInitHandler, handleShoot } = useLikeConfetti({
+    isLiked,
+    isUpdating,
+  });
 
   return (
     <form
---
+++ src/hooks/useOptimisticLike.tsx
@@ -0,0 +1,68 @@
+import { useState, useCallback, useOptimistic } from "react";
+import { likeOrUnlikeRepository } from "@/actions/likerepository.action";
+import { hasLikedRepository } from "@/lib/utils";
+import type { Repository, User } from "@/types/prisma.type";
+import type { Like } from "@prisma/client";
+
+type UseOptimisticLikeProps = {
+  user: User | null;
+  repository: Repository;
+  likes: Like[];
+};
+
+export const useOptimisticLike = ({
+  user,
+  repository,
+  likes,
+}: UseOptimisticLikeProps) => {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [optimisticLikes, setOptimisticLikes] = useOptimistic(
+    likes,
+    (state, newLike: Like) => {
+      if (
+        state.some((like) => hasLikedRepository(like, user?.id, repository.id))
+      ) {
+        return state.filter(
+          (like) => !hasLikedRepository(like, user?.id, repository.id),
+        );
+      }
+
+      return [...state, newLike];
+    },
+  );
+
+  const isLiked = optimisticLikes.some(
+    (like) => like.userId === user?.id && like.repositoryId === repository.id,
+  );
+
+  const handleLikeRepository = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (!user || isUpdating) return;
+
+      setIsUpdating(true);
+
+      try {
+        setOptimisticLikes({
+          userId: user.id,
+          repositoryId: repository.id,
+          id: Math.random(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+
+        await likeOrUnlikeRepository({
+          repositoryId: repository.id,
+        });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsUpdating(false);
+      }
+    },
+    [user, isUpdating, repository.id, setOptimisticLikes],
+  );
+
+  return { isUpdating, optimisticLikes, isLiked, handleLikeRepository };
+};
