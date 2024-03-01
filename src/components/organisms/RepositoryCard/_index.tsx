c1d3e9a60a4a851be821b11ccbeb63d47cf05121
SwiichyCode <adlpromail@gmail.com>
2024-03-01 02:04:24 +0100

    style: fix responsive of repositories grid

--- src/components/organisms/RepositoryCard/_index.tsx
+++ src/components/organisms/RepositoryCard/_index.tsx
@@ -1,3 +1,4 @@
+import { RepositoryCardLayout } from "./_layout";
 import { RepositoryCardHeader } from "./RepositoryCardHeader";
 import { RepositoryCardDescription } from "./RepositoryCardDescription";
 import { RepositoryCardFooter } from "./RepositoryCardFooter";
@@ -11,13 +12,13 @@ type Props = {
 
 export const RepositoryCard = ({ user, repository }: Props) => {
   return (
-    <div className="flex flex-col justify-between space-y-4 overflow-hidden rounded-md border border-card bg-default p-2 shadow">
+    <RepositoryCardLayout>
       <RepositoryCardHeader user={user} repository={repository} />
       <div className="space-y-2 rounded-sm bg-overlay p-3">
         <RepositoryCardDescription repository={repository} />
 
         <RepositoryCardFooter user={user} repository={repository} />
       </div>
-    </div>
+    </RepositoryCardLayout>
   );
 };
