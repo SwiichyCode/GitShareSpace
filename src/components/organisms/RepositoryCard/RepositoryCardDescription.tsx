e700b6e3172df75a0bcc1d5f2084e9690a826347
SwiichyCode <adlpromail@gmail.com>
2024-03-01 02:20:41 +0100

    fix: implement regex function for retrieve unsupported emoji

--- src/components/organisms/RepositoryCard/RepositoryCardDescription.tsx
+++ src/components/organisms/RepositoryCard/RepositoryCardDescription.tsx
@@ -1,3 +1,4 @@
+import { removeUnsupportedEmojis } from "@/utils/removeUnsupportedEmojis";
 import type { Repository } from "@/types/prisma.type";
 import { cn } from "@/lib/utils";
 
@@ -13,13 +14,20 @@ type DescriptionProps = {
 
 const Description = (props: DescriptionProps) => {
   const { title, text, className } = props;
-  const isEmptyClass = !text && "h-[40px] rounded-md bg-[#0D1117]";
+  const isEmptyClass = !text && "rounded-md bg-[#0D1117]";
 
   return (
-    <p className={cn("line-clamp-2 w-full text-sm", isEmptyClass, className)}>
+    <p
+      className={cn(
+        "line-clamp-2 h-[40px] w-full text-sm",
+        isEmptyClass,
+        className,
+      )}
+    >
       {text && (
         <>
-          <span className="font-semibold">{title}:</span> {text}
+          <span className="font-semibold">{title}:</span>{" "}
+          {removeUnsupportedEmojis(text)}
         </>
       )}
     </p>
