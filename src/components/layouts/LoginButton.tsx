72005af7eaf5bfc70d824407a3c77f838fdfb835
SwiichyCode <adlpromail@gmail.com>
2024-03-11 15:17:40 +0100

    refactor: implement authentication loading state

--- src/components/layouts/LoginButton.tsx
+++ src/components/layouts/LoginButton.tsx
@@ -1,14 +1,26 @@
 "use client";
+import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 
 export const LoginButton = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await signIn("github");
+    !response && setLoading(true);
+  };
+
   return (
-    <Button
-      onClick={async () => await signIn("github")}
-      className="hover:bg-success-hover bg-success space-x-2"
-    >
-      <span>Sign in</span>
-    </Button>
+    <form onSubmit={handleLogin}>
+      <Button
+        type="submit"
+        className="hover:bg-success-hover space-x-2 bg-success"
+      >
+        <span>{loading ? "Loading..." : "Sign in"}</span>
+      </Button>
+    </form>
   );
 };
